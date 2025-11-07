204999b51dc7e495b91aca26aff3adc346d7d82e
hellokitty <133889145+h3ll0k1tt7@users.noreply.github.com>
2025-11-07 06:09:18 +0200

    Add files via upload

--- personal portfolio/javascript.js
+++ personal portfolio/javascript.js
@@ -1,3 +1,20 @@
 function privacypolicy() {
     alert("Your privacy matters ❤ No cookies. No trackers. No data collection. Messages sent through my contact form are handled by Formspree and used only to reply. I never sell or share you information.");
 }
+  const music = document.getElementById("bgMusic");
+  const btn = document.getElementById("musicBtn");
+
+  btn.addEventListener("click", async () => {
+    try {
+      if (music.paused) {
+        await music.play();
+        music.volume = 0.3;
+        btn.textContent = "Pause Music ⏸";
+      } else {
+        music.pause();
+        btn.textContent = "Play Music 🎵";
+      }
+    } catch (err) {
+      console.log("Audio blocked:", err);
+    }
+  });

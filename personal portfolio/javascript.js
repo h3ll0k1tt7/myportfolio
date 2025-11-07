db1cd6baa1c5c9d99ee589265ad6c1cf6d7bce4b
hellokitty <133889145+h3ll0k1tt7@users.noreply.github.com>
2025-11-07 02:53:49 +0200

    Add files via upload

---
+++ personal portfolio/javascript.js
@@ -0,0 +1,3 @@
+function privacypolicy() {
+    alert("Your privacy matters ❤ No cookies. No trackers. No data collection. Messages sent through my contact form are handled by Formspree and used only to reply. I never sell or share you information.");
+}
